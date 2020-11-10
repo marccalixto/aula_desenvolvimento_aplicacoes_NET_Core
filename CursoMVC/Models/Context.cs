9f4bd7ec2c0e53836736de279d8c7b2611701f48
Marcelo Calixto <marccalixto@hotmail.com>
2020-11-10 00:12:54 -0400

    Projeto de estudo sobre .Net Core

---
+++ CursoMVC/Models/Context.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CursoMVC.Models
+{
+    public class Context : DbContext
+    {
+        public virtual DbSet<Categoria> Categorias { get; set; }
+        public virtual DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(LocalDb)\MSSQLLocalDB; Database=Cursomvc;Integrated Security=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public virtual void SetModified(object entity)
+        {
+            Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
---
+++ CursoMVC/Models/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "O campo é obrigatório")]
+        public string Descricao { get; set; }
+        public int Quantidade { get; set; }
+
+        [Display(Name = "Categoria")]
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+}
